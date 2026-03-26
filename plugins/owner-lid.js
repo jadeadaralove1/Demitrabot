7894b61c11617bf489118c8bc3af4840428e6b87
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 01:36:46 -0300

    Crear owner-lid.js

---
+++ plugins/owner-lid.js
@@ -0,0 +1,31 @@
+const handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, {
+    react: { text: '🛰️', key: m.key }
+  })
+
+  const context = m.message?.extendedTextMessage?.contextInfo
+  const objetivo = context?.participant || m.sender
+
+  const esLID = objetivo.endsWith('@lid')
+  const tipo = esLID
+    ? 'LID oculto (@lid)'
+    : 'Número visible (@s.whatsapp.net)'
+
+  const numero = objetivo.replace(/\D/g, '')
+
+  const mensaje = `
+📡 *Información del usuario detectado:*
+👤 *Identificador:* ${objetivo}
+📱 *Número:* +${numero}
+🔐 *Tipo de cuenta:* ${tipo}
+`.trim()
+
+  await m.reply(mensaje)
+  await m.reply(objetivo)
+}
+
+handler.help = ["𝖬𝗒𝗅𝗂𝖽"]
+handler.tags = ['owner']
+handler.command = ['lid', 'mylid', 'tulid']
+handler.group = true
+export default handler
