759fab80ca69e4ee9e6bf583999d00d42e2bda10
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 09:54:01 -0300

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,20 +1,20 @@
 import { exec } from 'child_process';
 
 const handler = async (m, { conn }) => {
-    let sentMsg = await m.reply('💢 Actualizando para mi darling... espera un momento~ 🌸');
+    let sentMsg = await m.reply('💢 Actualizando para ti... espera un momento');
 
     exec('git pull', (err, stdout, stderr) => {
         if (err) {
-            conn.sendMessage(m.chat, { text: '💔 Hmph... algo salió mal, darling. Déjame intentarlo a la fuerza~ 💢', edit: sentMsg.key }, { quoted: m });
+            conn.sendMessage(m.chat, { text: 'Mm... algo salió mal. Déjame intentarlo a la fuerza 💢', edit: sentMsg.key }, { quoted: m });
 
             exec('git reset --hard origin/main && git pull', (err2, stdout2, stderr2) => {
                 if (err2) {
-                    conn.sendMessage(m.chat, { text: `💔 Ni siquiera yo pude lograrlo, darling...\nRazón: ${err2.message}`, edit: sentMsg.key }, { quoted: m });
+                    conn.sendMessage(m.chat, { text: `Ni siquiera yo pude lograrlo...\nRazón: ${err2.message}`, edit: sentMsg.key }, { quoted: m });
                     return;
                 }
 
                 if (stderr2) console.warn(stderr2);
-                conn.sendMessage(m.chat, { text: `🌸 Lo hice a mi manera y funcionó, darling~\n\n${stdout2}`, edit: sentMsg.key }, { quoted: m });
+                conn.sendMessage(m.chat, { text: `Lo hice a mi manera y funcionó\n\n${stdout2}`, edit: sentMsg.key }, { quoted: m });
             });
             return;
         }
@@ -22,9 +22,9 @@ const handler = async (m, { conn }) => {
         if (stderr) console.warn(stderr);
 
         if (stdout.includes('Already up to date.')) {
-            conn.sendMessage(m.chat, { text: '🍬 Todo ya estaba en orden, darling~ No había nada que actualizar.', edit: sentMsg.key }, { quoted: m });
+            conn.sendMessage(m.chat, { text: 'Todo ya estaba en orden, No había nada que actualizar.', edit: sentMsg.key }, { quoted: m });
         } else {
-            conn.sendMessage(m.chat, { text: `🌸 Actualización completada con éxito, darling~!\n\n${stdout}`, edit: sentMsg.key }, { quoted: m });
+            conn.sendMessage(m.chat, { text: `Actualización completada con éxito!\n\n${stdout}`, edit: sentMsg.key }, { quoted: m });
         }
     });
 };
