cfdd63f74bd88ab739ad7884b0fe49b60ac0c319
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 01:46:31 -0300

    Crear tools-toimg.js

---
+++ plugins/tools-toimg.js
@@ -0,0 +1,37 @@
+import { downloadMediaMessage } from '@whiskeysockets/baileys'
+
+let handler = async (m, { conn }) => {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || q.mimetype || ''
+
+    if (!mime || !/webp/.test(mime)) {
+        await m.react('🌸')
+        return m.reply('💗 *Responde a un sticker* darling\~ para convertirlo en foto normal\n\nEjemplo: responde al sticker y escribe #toimg')
+    }
+
+    await m.react('🍬')
+
+    try {
+        let media = await downloadMediaMessage(q, 'buffer', {}, {
+            reuploadRequest: conn.updateMediaMessage
+        })
+
+        await conn.sendMessage(m.chat, {
+            image: media,
+            caption: '💗 ¡Aquí tienes tu imagen darling!\nConvertido con todo mi amor de Zero Two 🌸'
+        }, { quoted: m })
+
+        await m.react('💗')
+
+    } catch (e) {
+        console.error('❌ TOIMG ERROR:', e)
+        await m.react('💔')
+        m.reply('💔 Uy darling... este sticker se resistió un poquito\~\nPrueba con otro no me dejes sola 🌸')
+    }
+}
+
+handler.help = ['toimg', 'toimage', 'img']
+handler.tags = ['tools', 'stickers']
+handler.command = ['toimg', 'toimage', 'img']
+
+export default handler
