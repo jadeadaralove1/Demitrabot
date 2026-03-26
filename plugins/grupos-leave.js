4aa8d2005a58fc246cf504d9a028064423f58cdd
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-26 10:44:30 -0300

    Actualizar grupos-leave.js

--- plugins/grupos-leave.js
+++ plugins/grupos-leave.js
@@ -6,10 +6,9 @@ const handler = async (m, { conn }) => {
     await conn.sendMessage(m.chat, {
         image: { url: 'https://i.imgur.com/0Z2vY6L.jpeg' },
         caption:
-            `🌸💗 *¡KYAAAAAH~!! Me voy...* 💗🌸\n\n` +
-            `No lloren por mí, mis Darlings~ 😼\n\n` +
-            `Fue un honor volar con ustedes en mi Franxx ♡\n` +
-            `❝ Hasta la próxima... si es que vuelvo 🌷💗 ❞`
+            `Me voy...*\n\n` +
+            `No lloren por mí 😼\n\n` +
+            `❝ Hasta la próxima... si es que vuelvo 🫡🐢 ❞`
     })
 
     await conn.groupLeave(m.chat)
