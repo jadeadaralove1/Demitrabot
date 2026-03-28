66b6c19364cf4e464c53112a2fc6dc5c5414cde1
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-28 02:42:54 -0300

    Actualizar perfil-setdesc.js

--- plugins/perfil-setdesc.js
+++ plugins/perfil-setdesc.js
@@ -1,22 +1,19 @@
 import { database } from '../lib/database.js'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  // ✅ FIX DB
+let handler = async (m, { args, usedPrefix, command }) => {
   global.db = global.db || {}
   global.db.data = global.db.data || {}
   global.db.data.users = global.db.data.users || {}
 
   const userId = m.sender
-  // Inicializar usuario si no existe
   if (!global.db.data.users[userId]) {
     global.db.data.users[userId] = {
       name: 'Sin nombre',
       genre: 'Oculto',
       description: '',
       pasatiempo: 'No definido',
-      exp: 0,
       level: 0,
+      exp: 0,
       coins: 0,
       bank: 0,
       marry: null
@@ -24,18 +21,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   }
 
   const user = global.db.data.users[userId]
-
   const input = args.join(' ').trim()
-  if (!input) return m.reply(
-    `ᘛ  ࣭   🪼⃝    ࣪   ࣭Debes especificar una descripción válida para tu perfil.\n\n> Ejemplo » *${usedPrefix + command} Hola, uso WhatsApp!*`
-  )
 
-  // Guardar descripción
+  if (!input) return m.reply(`ᘛ  ࣭   🪼⃝    ࣪   ࣭Debes especificar una descripción válida.\n\n> Ejemplo » *${usedPrefix + command} Me encanta WhatsApp!*`)
+
   user.description = input
-
-  return m.reply(
-    `🐢⃝ Se ha establecido tu descripción correctamente.\nPuedes revisarla con *${usedPrefix}perfil* ૮₍˶• ˔ ก₎ა`
-  )
+  return m.reply(`✅ Se ha establecido tu descripción. Revisa tu perfil con *${usedPrefix}perfil*`)
 }
 
 handler.help = ['setdescription', 'setdesc']
