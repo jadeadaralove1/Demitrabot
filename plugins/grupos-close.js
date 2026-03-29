e5e35bd04a7f35bc5e6f8e6ed3323e73df5a6a26
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-29 19:33:04 -0300

    Actualizar grupos-close.js

--- plugins/grupos-close.js
+++ plugins/grupos-close.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
     await conn.groupSettingUpdate(m.chat, 'announcement')
-    m.reply('(＼᳟ㅤ ⃞🔒ㅤ.ᐟ   𝗚𝗿𝗎𝗉𝗈 𝖼𝖾𝗋𝗋𝗮𝗱𝗼.\n>
+    m.reply('(＼᳟ㅤ ⃞🔒ㅤ.ᐟ   𝗚𝗿𝗎𝗉𝗈 𝖼𝖾𝗋𝗋𝗮𝗱𝗼.
 ✧  ׁ    𓈒    
 > 𝗦𝖾 𝗰𝗲𝗋𝗋𝗈́ 𝖾𝗅 𝗀𝗋𝗎𝗉𝗈, 𝖠𝗁𝗈𝗋𝖺 𝗌𝗈𝗅𝗈 𝗅𝗈𝗌 𝖺𝖽𝗆𝗂𝗇𝗌 𝗉𝘂𝗲𝖽𝖾𝗇 𝗁𝗮𝗯𝗹𝗮𝗋. 
 > 𝖮 𝖾𝗌 𝗁𝗈𝗋𝖺 𝖽𝖾 𝗱𝖾𝗌𝖼𝖺𝗇𝗌𝗮𝗿.')
