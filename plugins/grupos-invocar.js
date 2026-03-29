1b495ac1b57484763b557d7165f02a2ba8cdb948
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-29 12:50:12 -0300

    Actualizar grupos-invocar.js

--- plugins/grupos-invocar.js
+++ plugins/grupos-invocar.js
@@ -12,7 +12,11 @@ const handler = async (m, { conn, args, isAdmin, isOwner }) => {
         const caption =
             ````       ````︶𖫲͜⏝ְ〬𔓕    🐞્    𔓕ְ〬︶𖫲͜⏝
 𞅀፝֟͜͝𞅀    🦭⃞͝ ⚭   ׅ   𝗜𝗡𝗩𝗢𝗖𝗔𝗡𝗗𝗢    ׂ     ׂ  🫐̼̼ᩙ\n\n` +
-            `*Anuncio:* ${anuncio}\n\n` +
+            `
+
+𓏲 ֶָ  ׄ⃟★֗𐚱 ˳  ׁ  ✷̱ ׅ ᭡ 𝗤𝘂𝖾𝗋𝗂𝖽𝗈𝗌 𝗈𝗒𝗲𝗻𝗍𝖾𝗌 𝖺𝗎𝗌𝖾𝗇𝗍𝖾𝗌… 𝖾𝗅 𝗀𝗋𝗎𝗽𝗼 𝗌𝗎𝖾𝗇𝖺 𝗆𝖺́𝗌 𝗆𝗎𝖾𝗋𝘁𝗼 𝗊𝗎𝖾 𝗎𝗇𝖺 𝗋𝖺𝖽𝗂𝗈 𝗌𝗶𝗻 𝗌𝖾𝗇̃𝖺𝗅.
+𝖠𝖼𝗍𝗂́𝗏𝖾𝗇𝗌𝖾, 𝗁𝖺𝗀𝖺𝗇 𝗋𝗎𝗂𝖽𝗈, 𝗿𝗲𝗌𝗉𝗂𝗋𝖾𝗇 𝖺𝗎𝗇𝗊𝗎𝖾 𝗌𝖾𝖺 𝗉𝗈𝗋 𝖼𝗈𝗆𝗉𝗿𝗼𝗺𝗂𝗌𝗈.
+> ${anuncio}\n\n` +
             `${mentions}\n\n` +
             `¡Respondan rapido no me dejen sola esperando!`
 
