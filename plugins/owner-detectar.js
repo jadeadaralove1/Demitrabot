34907867fd6b322d47262e98deaa7d52407698f1
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-29 13:49:25 -0300

    Actualizar owner-detectar.js

--- plugins/owner-detectar.js
+++ plugins/owner-detectar.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn }) => {
 
                 response += `*Error encontrado*\n\n`
                 response += `◜࣭࣭࣭࣭࣭᷼📁̸̷ׁᮬᰰᩫ࣭࣭࣭࣭ *Archivo:* ${file}\n`
-                response += `📝 *Mensaje:* ${error.message}\n`
+                response += `◜࣭࣭࣭࣭࣭᷼📝̸̷ׁᮬᰰᩫ࣭࣭࣭࣭ *Mensaje:* ${error.message}\n`
                 response += `📍 *Línea:* ${errorLine}\n`
                 response += `━━━━━━━━━━━━━━━━━━━\n\n`
             }
