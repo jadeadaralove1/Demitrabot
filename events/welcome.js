64e97170b7d99ec955bb6ba336a08c2087673d01
jadeadaralove1 <helpsiris1@gmail.com>
2026-03-29 22:52:24 -0300

    Actualizar welcome.js

--- events/welcome.js
+++ events/welcome.js
@@ -22,22 +22,22 @@ export const run = async (conn, update) => {
             const user = participant.split('@')[0]
 
             let texto = `
-   ๑     𓈒   ⃝🔔   ׅ   🄳ιɳɠ 🄳σɳɠ       ᮫     ୨୧
-       ￮   ֹ 🦭⃞    ࣭    𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝗂𝖽𝗑 ✨ྀ  .  ♡
+ ๑     𓈒   ⃝🔔   ׅ   🄳ιɳɠ 🄳σɳɠ       ᮫     ୨୧
+     ￮   ֹ 🦭⃞    ࣭    𝖡𝗂𝖾𝗇𝗏𝖾𝗇𝗂𝖽𝗑 ✨ྀ  .  ♡
 
 ʚֵ    ۟    ﹠  🌟̷̷ 𝖣𝖾𝗆𝗂𝗍𝗋𝖺 𝖻𝗈𝗍 𝖽𝖾 𝗐𝗁𝖺𝗍𝗌𝖺𝗉𝗉
                                 𝖧𝖺𝗉𝗉𝗒 ࣪ 🪼  ּ    𝅄  𔘓
 
-     ଓ     ๋    ࣪   🌻⃞      ࣭   @user ࣪      ੭     •
+    ଓ     ๋    ࣪   🌻⃞      ࣭   @user ࣪      ੭     •
 
 
 🪷  ㅤ＇   ❚ ❘ Saludos soy Demitra un bot disponible para cualquier momento, y será un placer recibirte en este lugar donde cada presencia cuenta.
 
-᷼⥃ᩥ🤖ׅ֟፝͠ᩬ⃞    ﹗ Te doy la bienvenida; espero que tu paso por aquí resulte agradable y deje una buena impresión.
+᷼⥃ᩥ🤖ׅ֟፝͠ᩬ⃞ Te doy la bienvenida; espero que tu paso por aquí resulte agradable y deje una buena impresión.
 
-᷼⥃ᩥ🧝🏽‍♀️ׅ֟፝͠ᩬ⃞    ﹗Puedes tomar tu lugar, observar con calma y participar cuando lo desees… siempre hay algo interesante por descubrir.  Usa "#menu" para más información.
+> Puedes tomar tu lugar, observar con calma y participar cuando lo desees… siempre hay algo interesante por descubrir.  Usa "#menu" para más información.
 
-💎 ㅤ＇   ❚ ❘  @desc
+＇❚ ❘  @desc
 
 ㅤ୨ৎㅤ⏜︵⏜  ⪩ ⪨  ⏜︵⏜୨ৎ
 
